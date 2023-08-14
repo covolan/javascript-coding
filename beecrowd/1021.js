b5189c86bcbf90da29e49d6eb44b007ee00da0b5
covolan <covolan09@gmail.com>
2023-08-14 20:30:24 -0300

    Corrigido exercicio 1021

--- beecrowd/1021.js
+++ beecrowd/1021.js
@@ -1,26 +1,31 @@
 var input = require('fs').readFileSync('./dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-let valor = Math.trunc(Number(input));
-let valorMoedas = Number(input) - valor;
-let notas = [100.00, 50.00, 20.00, 10.00, 5.00, 2.00];
-let moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01];
+let nota = Math.trunc(Number(input));
+let moeda = Number(input) - Math.trunc(Number(input));
 
-console.log("NOTAS:");
+let notaValores = [100, 50, 20, 10, 5, 2];
+let moedaValores = [1, 0.50, 0.25, 0.10, 0.05, 0.01];
 
-for (let i = 0; i < notas.length; i++) {
-    let temp = valor % notas[i];
-    let tot = (valor - temp) / notas[i];
-    console.log(tot + " nota(s) de R$ " + notas[i].toFixed(2));
-    valor -= notas[i] * tot;
+console.log("NOTAS:")
+
+for (let i = 0; i < notaValores.length; i++) {
+    let temp = 0;
+    if (nota > notaValores[i]) {
+        temp = (nota - (nota % notaValores[i])) / notaValores[i];
+        nota = nota - (temp * notaValores[i]);
+    }
+    console.log(temp + " nota(s) de R$ " + notaValores[i].toFixed(2));
 }
 
-console.log("MOEDAS:");
-valor += valorMoedas;
+console.log("MOEDAS:")
+moeda += nota;
 
-for (let i = 0; i < moedas.length; i++) {
-    let temp = valor % moedas[i];
-    let tot = (valor - temp) / moedas[i];
-    console.log(tot + " moeda(s) de R$ " + moedas[i].toFixed(2));
-    valor -= moedas[i] * tot;
+for (let i = 0; i < moedaValores.length; i++) {
+    let temp = 0;
+    if (moeda > moedaValores[i]) {
+        temp = (moeda - (moeda % moedaValores[i])) / moedaValores[i];
+        moeda = moeda - (temp * moedaValores[i]) + 0.00001;
+    }
+    console.log(temp.toFixed(0) + " moeda(s) de R$ " + moedaValores[i].toFixed(2));
 }
