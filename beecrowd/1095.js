08bd4b6d4c1e0b55bf09b0bdc697d71aee670fd4
Alexandre Covolan <82233795+covolan@users.noreply.github.com>
2023-08-19 20:49:56 -0300

    correction on problem 1095

--- beecrowd/1095.js
+++ beecrowd/1095.js
@@ -1,6 +1,15 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
 let j = 60;
 
-for (let i = 1; j >= 0; i++) {
-    console.log("i = " + i + " j = " + j);
+for (let i = 1; j >= 0; i+=3) {
+    console.log("I=" + i + " J=" + j);
     j -= 5;
 }
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
